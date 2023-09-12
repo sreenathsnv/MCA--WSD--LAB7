922f311e8c72757d67832cb3f73de722979e9415
Sreenath V <sreenathsnvaltpb@gmail.com>
2023-09-12 21:50:58 +0530

    Bugs fixed

--- script.js
+++ script.js
@@ -13,44 +13,28 @@ function fetchData()
 
     xhr.onload = function(){
 
-      if(this.readystate == 4)
-      {
         if(this.status == 200)
       {
         let response = JSON.parse(this.responseText);
 
         response.forEach(element => {
 
-            booksDiv.innerHTML += `
+          booksDiv.innerHTML += `
             
-            <div class="card" style="width: 18rem;">
-  <img src="${element.imageLink}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${element.title}</h5>
-    <p class="card-text"> written by ${element.author}</p>
-    <a href="#" class="btn btn-primary">${element.link}</a>
-  </div>
+          <div class="card" style="width: 25vw; height:10vw; margin:1vh;">
+<div class="card-body">
+  <h5 class="card-title">${element.title}</h5>
+  <p class="card-text"> written by ${element.author}</p>
+  <a href="#" class="btn btn-primary">${element.link}</a>
 </div>
-            `
-            
-        });
-        
+</div>
+          `
+          
+      });
       }
       else{booksDiv.innerHTML = '<p style = "margin-left:35vw;font-size : 4vh;color:red;">Some Error Occured!!</p>'}
-      }
-      else{booksDiv.innerHTML = '<p style = "margin-left:35vw;font-size : 4vh;color:red;">Failed to load the content</p>'}
-      
-
 
     }
     xhr.send()
 }
 
-{/* <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div> */}
